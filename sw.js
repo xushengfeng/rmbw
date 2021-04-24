340cb9a7b28c91c535c3649955844114c1308464
xsf <xushengfeng_zg@163.com>
2021-04-24 20:27:13 +0800

    a

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ self.addEventListener('install', event => {
         caches.open(CACHE_NAME)
           .then(cache => cache.addAll(
             [
-                '/',  // 这个一定要包含整个目录，不然无法离线浏览
+                '/rmbw/',  // 这个一定要包含整个目录，不然无法离线浏览
                 'asset/icon.png',
                 'index.html',
                 'css/css.css'
